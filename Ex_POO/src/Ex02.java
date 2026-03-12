566e43c246986c9701b5338745271dfc39876988
Gabriel Carvalho <gabrielcarvalhoggomes@gmail.com>
2026-03-12 19:33:05 -0300

    adiciona Ex02

---
+++ Ex_POO/src/Ex02.java
@@ -0,0 +1,36 @@
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		
+				Mercado unidadeBlumenau = new Mercado();
+
+				unidadeBlumenau.nomeMercado = "Giassi";
+				unidadeBlumenau.qtdMacasVendidas = 500;
+				unidadeBlumenau.precoMacas = 8.7;
+				unidadeBlumenau.qtdLaranjasVendidas = 400;
+				unidadeBlumenau.precoLaranjas = 9.25;
+
+				
+				Mercado unidadeJoinville = new Mercado();
+
+				unidadeJoinville.nomeMercado = "Angeloni";
+				unidadeJoinville.qtdMacasVendidas = 600;
+				unidadeJoinville.precoMacas = 8.9;
+				unidadeJoinville.qtdLaranjasVendidas = 300;
+				unidadeJoinville.precoLaranjas = 10.25;
+
+				
+				Mercado unidadeFlorianopolis = new Mercado();
+
+				unidadeFlorianopolis.nomeMercado = "Bistek";
+				unidadeFlorianopolis.qtdMacasVendidas = 700;
+				unidadeFlorianopolis.precoMacas = 7.7;
+				unidadeFlorianopolis.qtdLaranjasVendidas = 500;
+				unidadeFlorianopolis.precoLaranjas = 11.25;
+
+
+	}
+
+}
---
+++ Ex_POO/src/Mercado.java
@@ -0,0 +1,10 @@
+
+public class Mercado {
+
+	String nomeMercado;
+	int qtdMacasVendidas;
+	double precoMacas;
+	int qtdLaranjasVendidas;
+	double precoLaranjas;
+
+}
